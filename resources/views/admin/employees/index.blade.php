27a99199063be1ffe38633c13a9fe0aa58ea7418
junaid10521 <junaidonlygia@gmail.com>
2021-06-25 01:33:32 +0500

    first commit

---
+++ resources/views/admin/employees/index.blade.php
@@ -0,0 +1,109 @@
+@extends('admin.layout.app')
+
+@section('title', 'Users')
+
+@section('content')
+    <div class="content-wrapper">
+        <section class="content">
+            <div class="container-fluid">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Employees List</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Rendering engine</th>
+                                <th>Browser</th>
+                                <th>Platform(s)</th>
+                                <th>Engine version</th>
+                                <th>CSS grade</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Trident</td>
+                                <td>Internet
+                                    Explorer 5.5
+                                </td>
+                                <td>Win 95+</td>
+                                <td>5.5</td>
+                                <td>A</td>
+                                <td>
+                                    <button class="btn btn-sm btn-warning ">Edit</button>
+                                    <button class="btn btn-sm btn-danger ">Delete</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Trident</td>
+                                <td>Internet
+                                    Explorer 6
+                                </td>
+                                <td>Win 98+</td>
+                                <td>6</td>
+                                <td>A</td>
+                                <td>
+                                    <button class="btn btn-sm btn-warning ">Edit</button>
+                                    <button class="btn btn-sm btn-danger ">Delete</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Trident</td>
+                                <td>Internet
+                                    Explorer 6
+                                </td>
+                                <td>Win 98+</td>
+                                <td>6</td>
+                                <td>A</td>
+                                <td>
+                                    <button class="btn btn-sm btn-warning ">Edit</button>
+                                    <button class="btn btn-sm btn-danger ">Delete</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Trident</td>
+                                <td>Internet
+                                    Explorer 6
+                                </td>
+                                <td>Win 98+</td>
+                                <td>6</td>
+                                <td>A</td>
+                                <td>
+                                    <button class="btn btn-sm btn-warning ">Edit</button>
+                                    <button class="btn btn-sm btn-danger ">Delete</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Trident</td>
+                                <td>Internet
+                                    Explorer 6
+                                </td>
+                                <td>Win 98+</td>
+                                <td>6</td>
+                                <td>A</td>
+                                <td>
+                                    <button class="btn btn-sm btn-warning ">Edit</button>
+                                    <button class="btn btn-sm btn-danger ">Delete</button>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+            </div>
+    </div>
+    </div>
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('dashboard','admin\dashboard\DashboardController@index');
+Route::prefix('users')->group(function () {
+    Route::get('/users-list', function () {
+        return view('admin.users.index');
+    });
+});
+
+Route::prefix('roles')->group(function () {
+    Route::get('roles-list','admin\roles\RolesController@index');
+});
+
+Route::prefix('employees')->group(function () {
+    Route::get('employees-list','admin\employees\EmployeesController@index');
+});
+
+Route::prefix('companies')->group(function () {
+    Route::get('companies-list','admin\companies\CompaniesController@index');
+});
+
+Route::prefix('permissions')->group(function () {
+    Route::get('permissions-list','admin\permissions\PermissionsController@index');
+});
